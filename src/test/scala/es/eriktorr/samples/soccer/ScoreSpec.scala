e784aa0e0712573ec4f4fa1b5b07a5747e9571a4
Erik Torres <erik.torres@lifullconnect.com>
2020-02-09 14:59:33 +0100

    :construction: Compute team score after the match

--- src/test/scala/es/eriktorr/samples/soccer/ScoreSpec.scala
+++ src/test/scala/es/eriktorr/samples/soccer/ScoreSpec.scala
@@ -8,20 +8,22 @@ import org.apache.spark.sql.functions.udf
 class ScoreSpec extends SetupDataset {
   "test" should "work" in {
     import spark.implicits._
-
     DatasetReader[Match].datasetFrom(pathTo("data/match.csv.bz2"))
-      .withColumn("score", scoreFrom('away_team_goal, 'home_team_goal))
-      .select('id, 'country_id, 'league_id, 'date, 'home_team_api_id, 'away_team_api_id, 'home_team_goal, 'away_team_goal, $"score._1" as "home_team_score", $"score._2" as "away_team_score")
+      .withColumn("score", udfScore('home_team_goal, 'away_team_goal))
+      .select('id, 'country_id, 'league_id, 'date, 'home_team_api_id, 'away_team_api_id, 'home_team_goal, 'away_team_goal,
+        $"score._1" as "home_team_score", $"score._2" as "away_team_score")
       .filter('date >= Timestamp.valueOf("2011-08-01 00:00:00"))
-      .show(true)
+      .show(false)
 
     // TODO
 
   }
 
-  lazy val scoreFrom: UserDefinedFunction = udf((goalTeamA: Int, goalTeamB: Int) => (goalTeamA, goalTeamB) match {
-    case (goalTeamA, goalTeamB) if goalTeamA > goalTeamB => (3, 0)
-    case (goalTeamA, goalTeamB) if goalTeamA < goalTeamB => (0, 3)
+  val scoreFrom: (Int, Int) => (Int, Int) = (x: Int, y: Int) => (x, y) match {
+    case (x, y) if x > y => (3, 0)
+    case (x, y) if x < y => (0, 3)
     case _ => (1, 1)
-  })
+  }
+
+  lazy val udfScore: UserDefinedFunction = udf[(Int, Int), Int, Int](scoreFrom)
 }
